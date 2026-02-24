644c7a16442afa71901656cce3b5db53a11ae674
xiqing1345 <167727795+xiqing1345@users.noreply.github.com>
2026-02-23 23:53:47 -0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,9 +2,9 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact</h1>
-      <p>Email: <a href="mailto:example@example.com">example@example.com</a></p>
+      <p>Email: <a href="mailto:shizhuo@kean.edu">example@example.com</a></p>
       <p>
-        LinkedIn: <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">linkedin.com</a>
+        LinkedIn: <a href="https://www.linkedin.com/in/%E5%8D%93%E6%9D%AD-%E7%9F%B3-56aa94385/" target="_blank" rel="noreferrer">linkedin.com</a>
       </p>
     </div>
   );
