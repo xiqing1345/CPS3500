8568cc5e3a52c11843106962fd9f4e4dd6337b04
xiqing1345 <167727795+xiqing1345@users.noreply.github.com>
2026-02-24 00:00:37 -0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,10 +2,26 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact</h1>
-      <p>Email: <a href="mailto:shizhuo@kean.edu">example@example.com</a></p>
+
       <p>
-        LinkedIn: <a href="https://www.linkedin.com/in/%E5%8D%93%E6%9D%AD-%E7%9F%B3-56aa94385/" target="_blank" rel="noreferrer">linkedin.com</a>
+        Email: <a href="mailto:shizhuo@kean.edu">shizhuo@kean.edu</a>
       </p>
+
+      <p>
+        LinkedIn: 
+        <a 
+          href="https://www.linkedin.com/in/%E5%8D%93%E6%9D%AD-%E7%9F%B3-56aa94385/" 
+          target="_blank" 
+          rel="noreferrer"
+        >
+          linkedin.com
+        </a>
+      </p>
+
+      <p>
+        WeChat: ssz111q1
+      </p>
+
     </div>
   );
 }
